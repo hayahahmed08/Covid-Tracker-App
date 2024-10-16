6d2f2885aea279ab8a7d0950933729edcd0a25e9
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-10-17 02:52:32 +0500

    Update world_stats.dart

--- lib/View/world_stats.dart
+++ lib/View/world_stats.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class WorldStats extends StatefulWidget {
   const WorldStats({super.key});
-
 
   @override
   State<WorldStats> createState() => _WorldStatsState();
@@ -13,9 +13,95 @@ class _WorldStatsState extends State<WorldStats> with TickerProviderStateMixin {
     duration: const Duration(seconds: 3),
     vsync: this,
   )..repeat();
+  void  dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  final colorList = const <Color>[
+    Color(0xff4285f4),
+    Color(0xff1aa260),
+    Color(0xffde5246),
+  ]; // <Color>[]
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 00,
+            ),
+            PieChart(
+              dataMap: {"Total": 20, "Recovered": 15, "Deaths": 5},
+              chartRadius: MediaQuery.of(context).size.width / 3.2,
+              legendOptions:
+                  const LegendOptions(legendPosition: LegendPosition.left),
+              animationDuration: Duration(milliseconds: 1200),
+              chartType: ChartType.ring,
+              colorList: colorList,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: MediaQuery.of(context).size.height * .06,
+              ),
+              child: Card(
+                child: Column(
+                  children: [
+                    ReusableRow(title: "Total", value: "200"),
+                    ReusableRow(title: "Total", value: "200"),
+                    ReusableRow(title: "Total", value: "200")
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                  color: const Color(0xff1aa260),
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Center(
+                child: Text("Track Countries"),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+class ReusableRow extends StatelessWidget {
+  String title, value;
+
+  ReusableRow({Key? key, required this.title, required this.value})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 5),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title),
+                Text(value),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Divider(),
+          ],
+        ),
+      ),
+    );
   }
 }
