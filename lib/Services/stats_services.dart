b8bf3430d90bd65a666411f20cdd9b581e425e14
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-10-18 20:37:33 +0500

    fetched data from api

---
+++ lib/Services/stats_services.dart
@@ -0,0 +1,18 @@
+import 'package:covidapp/Services/Utilities/app_url.dart';
+import 'package:http/http.dart' as http;
+import 'package:covidapp/Model/WorldStatsModel.dart';
+import 'dart:convert';
+
+class StatsServices {
+
+  Future<WorldStatsModel> fetchWorldStatsRecords () async{
+final response = await http.get(Uri.parse(AppUrl.worldStatsApi));
+if (response.statusCode ==200){
+  var data = jsonDecode(response.body);
+  return WorldStatsModel.fromJson(data);
+
+}else{
+  throw Exception('Error');
+}
+  }
+}
